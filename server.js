55fc88abd0781d0448bf0410212456904a0c7828
KNA <knayad@gmail.com>
2024-05-22 20:42:59 -0400

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+
 // const cors = require("cors");
+const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 8000;
-
 const app = express();
+
+connectDB();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
