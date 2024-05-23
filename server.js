9e6462b085f16a4f9d9d4f3641bcb754f7795c87
KNA <knayad@gmail.com>
2024-05-22 21:59:44 -0400

    reverting to f72b016 due to break in Common JS vs importing node modules

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 
 // const cors = require("cors");
 const { connectDB } = require("./config/db");
+
 const PORT = process.env.PORT || 8000;
 const app = express();
-const userRoutes = require("./routes/userRoutes");
 
 connectDB();
 app.use(express.json());
@@ -13,7 +13,5 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
-
-app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
