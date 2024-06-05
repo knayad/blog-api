69150484fd2309ac69e34d9c013b23994757be01
KNA <knayad@gmail.com>
2024-06-04 21:29:07 -0400

    json updates, error handlers reverted

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -11,20 +11,24 @@ const registerUser = async (req, res, next) => {
     if (user) {
       throw new Error("User already exists with that email.");
     }
-    // create a new user if email is not found.
-    user = await User.create({ name, email, password });
 
-    return res.status(201).json({
-      _id: user._id,
-      avatar: user.avatar,
-      name: user.name,
-      email: user.email,
-      verified: user.verified,
-      admin: user.admin,
-      token: generateJWT,
-    });
+    // create a new user if email is not found.
+    if (!user) {
+      user = await User.create({ name, email, password });
+
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+        token: generateJWT,
+      });
+    }
   } catch (error) {
-    next(error);
+    // next(error);
+    return res.status(500).json({ message: "Something went wrong!" });
   }
 };
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const {
   errorResponseHandler,
   invalidPathHandler,
 } = require("./middleware/errorHandler");
+
 const userRoutes = require("./routes/userRoutes");
 
 const { connectDB } = require("./config/db");
@@ -14,14 +15,15 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 connectDB();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
 
-app.use(errorResponseHandler);
-app.use(invalidPathHandler);
+// app.use(errorResponseHandler);
+// app.use(invalidPathHandler);
 app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
